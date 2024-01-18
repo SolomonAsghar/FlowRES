bc9743102097533c08e4458810befd7809300506
SolomonAsghar <66965716+SolomonAsghar@users.noreply.github.com>
2024-01-18 16:53:42 +0000

    Update ABW_ProbEqs_Passive.py

--- software/ABW_ProbEqs_Passive.py
+++ software/ABW_ProbEqs_Passive.py
@@ -1,7 +1,16 @@
+'''
+Contains functions to work out probability equations required for FlowRes training and for calculating acceptance probability.
+'''
 import numpy as np
 import math
 
 def Target_Prob(Net, pos):
+    '''
+    Probability of the target distribution, i.e. pdf of the transition path distribution.
+
+    Net [FlowNet]: Network used to generate the paths.
+    pos [numpy array]: Array containing the positions we want to work out prob of. 
+    '''
     ### Append Starts
     starts = np.tile([Net.start], (len(pos),1,1))
     Positions = np.concatenate((starts[:,:,:2], pos), axis=1)
@@ -14,6 +23,12 @@ def Target_Prob(Net, pos):
     return np.exp(- 0.5 * np.sum(Translational_Noise**2, axis=(1,2)))
 
 def Log_Target_Prob(Net, pos):
+    '''
+    Log of probability of the target distribution, i.e. pdf of the transition path distribution.
+
+    Net [FlowNet]: Network used to generate the paths.
+    pos [numpy array]: Array containing the positions we want to work out prob of. 
+    '''    
     ### Append Starts
     starts = np.tile([Net.start], (len(pos),1,1))
     Positions = np.concatenate((starts[:,:,:2], pos), axis=1)
@@ -26,7 +41,17 @@ def Log_Target_Prob(Net, pos):
     return - 0.5 * np.sum(Translational_Noise**2, axis=(1,2))
 
 def Latent_Prob(pos):
+    '''
+    Probability of samples from the base distribution, in the passive case this is simply the Gaussian pdf. 
+
+    pos [numpy array]: Positions from base path that we want to work out the probability of.
+    '''
     return np.exp(- 0.5 * np.sum(pos**2, axis=(1,2)))
 
 def Log_Latent_Prob(pos):
+    '''
+    Log of probability of samples from the base distribution, in the passive case this is simply the Gaussian pdf. 
+
+    pos [numpy array]: Positions from base path that we want to work out the probability of.
+    '''
     return - 0.5 * np.sum(pos**2, axis=(1,2))
